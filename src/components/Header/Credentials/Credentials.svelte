03c14cbcb8b05f2db09e13e1cf14dd9acdaa22d7
rowlandwilliams <rowland.williams.94@gmail.com>
2022-11-27 14:44:48 +0000

    attempt prod image fix

--- src/components/Header/Credentials/Credentials.svelte
+++ src/components/Header/Credentials/Credentials.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	const credentials = [
-		{ icon: 'website.svg', credential: 'rowlandwilliams.me' },
-		{ icon: 'email.svg', credential: 'rowland.williams.94@gmail.com' },
-		{ icon: 'location.svg', credential: 'UK' }
+		{ icon: 'website', credential: 'rowlandwilliams.me' },
+		{ icon: 'email', credential: 'rowland.williams.94@gmail.com' },
+		{ icon: 'location', credential: 'UK' }
 	];
 </script>
 
 <div class="flex flex-col gap-y-0.5 rounded-sm p-2 text-xs">
 	{#each credentials as { icon, credential }}
 		<div class="flex items-center gap-x-2">
-			<div class="w-3 text-indigo-400"><img src={`/credentials/${icon}`} alt={icon} /></div>
+			<div class="w-3 text-indigo-400"><img src={`/credentials/${icon}.svg`} alt={icon} /></div>
 			<p class="text-gray-500">{credential}</p>
 		</div>
 	{/each}
